f9265642ddb0b0bd497d4992a1304f34a07134a1
Johan Sjöblom <sjoblomj88@gmail.com>
2024-01-10 18:35:22 +0100

    Adding script

---
+++ downloader.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+maxattempts=5
+
+function create_list_to_be_downloaded() {
+  filename="$1"
+  list=$(curl -s https://dumps.wikimedia.org/backup-index.html | cat | grep -v "(private data)" | grep "span class='done'" | sed -r 's/.*<a href="([^\/]+)\/([^"]+)">(.*)<\/a>.*/\1 \2/g' | tac)
+  echo "$list" | while read wiki date; do
+    echo "Fetching files to download for $wiki $date"
+    dlpage=$(curl -s https://dumps.wikimedia.org/$wiki/$date/ | grep "$wiki-$date-pages-meta-history.*\.7z" | sed -r 's/.*<a href="([^"]+)">([^<]+)<\/a>.*/\2 https:\/\/dumps.wikimedia.org\1/g')
+    echo "$dlpage" | while read file url; do
+      echo "$wiki $date $file $url" >> "$filename"
+    done
+  done
+}
+
+function remove_file_if_invalid() {
+  local file="$1"
+  local wiki="$2"
+  local date="$3"
+
+  if [ -f "$file" ]; then
+    if [[ $(sha1sum "$file") = $(curl -s "https://dumps.wikimedia.org/$wiki/$date/$wiki-$date-sha1sums.txt" | grep "$file") ]]; then
+      echo "Validating $file: ok"
+    else
+      rm "$file"
+      echo "Validating $file: NOK"
+    fi
+  fi
+}
+
+function download_from_list() {
+  filename="$1"
+  while read wiki date file url; do
+    remove_file_if_invalid "$file" "$wiki" "$date"
+
+    if [ ! -f "$file" ]; then
+      for i in $(seq 1 $maxattempts); do
+        echo ""
+        echo "${file}"
+        curl $url -o $file
+
+        if [[ $(remove_file_if_invalid "$file" "$wiki" "$date" | awk '{print $NF}') = "ok" ]]; then
+          break
+        else
+          echo "Attempt $i/$maxattempts: Failed to download $file"
+        fi
+      done
+    fi
+  done < "$filename"
+}
+
+dl_file="to_download.txt"
+create_list_to_be_downloaded "$dl_file"
+download_from_list "$dl_file"
